4b98c279ac0fcc0ad80c71319ed6705502e35acf
Shun Git <kwokshungit@gmail.com>
2021-09-01 19:48:32 +0800

    [api] use global state

--- lib/api.d.ts
+++ lib/api.d.ts
@@ -1,2 +1,2 @@
-type API = (path: string, params?: object, post?: object | boolean) => Promise<object>
-declare function create(token: string, clientId: string): API
+export function use(token: string, clientId: string): void
+export function raw(path: string, params?: object, post?: object | boolean): Promise<object>
