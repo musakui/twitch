c4eeea2bdc2d523f4f9c109561eab8302722986d
Shun Git <kwokshungit@gmail.com>
2021-09-02 22:12:44 +0800

    [chat] add message processors

--- lib/chat.d.ts
+++ lib/chat.d.ts
@@ -6,23 +6,60 @@ interface ChatOptions {
 }
 
 interface RoomState {
+  roomId: string
+  r9k?: string
+  slow?: string
+  rituals?: string
+  subsOnly?: string
   emoteOnly?: string
+  followersOnly?: string
 }
 
 interface ChatRoom {
   name: string
   state: RoomState
-  part (): void
-  say (text: string): void
+  part(): void
+  say(text: string): void
 }
 
-type EventName = 'ready' | 'chat' | 'info'
-type EventHandler = (details: any) => void
+interface ChatEvent {
+  command: string
+  raw?: string
+  chan?: string
+  user?: string
+  text?: string
+  tags?: Record<string, string>
+
+  // only for NAMES
+  join?: string[]
+  part?: string[]
+}
+
+type EventName = 'info' | 'chat'
+type EventHandler = (details: ChatEvent) => void
 type EventHandlerRemover = () => void
 
 declare class Chat extends WebSocket {
   constructor(opts?: ChatOptions)
+  connected: boolean
   user: string
   on(name: EventName, handler: EventHandler): EventHandlerRemover
   join(name: string): Promise<ChatRoom>
+}
+
+export function create(opts?: ChatOptions): Chat
+
+type ChatFragments = any[]
+type CheerProcessor = (fragments: ChatFragments) => ChatFragments
+
+interface CheermoteInfo {
+  prefix: string
+  tiers: object[]
+}
+
+export function createCheerProcessor(cheermotes: CheermoteInfo[]): CheerProcessor
+
+export function getFragments(message: { text: string, tags: object }): {
+  action?: boolean,
+  fragments: ChatFragments,
 }
