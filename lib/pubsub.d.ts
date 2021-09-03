74f5dc8cafce6b3f289b1740c47d857b4f69b017
Shun Git <kwokshungit@gmail.com>
2021-09-03 22:35:10 +0800

    [pubsub] add PubSub topics

--- lib/pubsub.d.ts
+++ lib/pubsub.d.ts
@@ -1,3 +1,5 @@
+type Topic = string
+
 type EventHandler = (details: any) => void
 type EventHandlerRemover = () => void
 
@@ -6,6 +8,16 @@ interface PubSubOptions {
 }
 
 declare class PubSub extends WebSocket {
-  constructor(token: string, topics: object, opts?: PubSubOptions)
+  constructor(token: string, topics: Record<string, Topic>, opts?: PubSubOptions)
   on(name: string, handler: EventHandler): EventHandlerRemover
+}
+
+export function create(token: string, topics: Record<string, Topic>, opts?: PubSubOptions): PubSub
+
+declare namespace Topics {
+  export function bits(id: string): Topic
+  export function bitsBadge(id: string): Topic
+  export function channelPoints(id: string): Topic
+  export function channelSubscriptions(id: string): Topic
+  export function whispers(id: string): Topic
 }
